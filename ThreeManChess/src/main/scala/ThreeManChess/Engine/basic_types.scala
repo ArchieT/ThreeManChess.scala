ec8bd8e526676b2f4e0914cf15c1e31b93981cdd
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-09-18 01:53:38 +0200

    working on basic types, still

--- ThreeManChess/src/main/scala/ThreeManChess/Engine/basic_types.scala
+++ ThreeManChess/src/main/scala/ThreeManChess/Engine/basic_types.scala
@@ -6,6 +6,8 @@ import refined.numeric.Positive
 import refined.collection.NonEmpty
 import refined.auto._
 
+import scala.None
+
 /**
   * Created by Michał Krzysztof Feiler on 30.08.18.
   */
@@ -48,6 +50,13 @@ object Pos {
   type SegmentEight = Int Refined Interval.Closed[W.`0`.T,W.`7`.T]
   type Rank = Int Refined Interval.Closed[W.`0`.T,W.`5`.T]
   type File = Int Refined Interval.Closed[W.`0`.T,W.`23`.T]
+  def opposite(a : File) : File = a<12 ? f+12 : f-12
+  def plus(a : File, b : Int Refined NonNegative) : File = (a+b)%24
+  def minus(a : File, b : Int Refined NonNegative) : File = Math.floorMod(a-b, 24)
+  def signSum(s : FilewiseDirection, a : File, b : Int Refined NonNegative) : File = s match {
+    case Pluswards => plus(a, b)
+    case Minuswards => minus(a, b)
+  }
 
   type ColorSegm = Color
 }
@@ -57,15 +66,65 @@ final class Pos(val rank: Rank, val file: File) {
   def segmFile() : SegmentEight = file%3
 }
 
-sealed trait LinearDirection
+trait Reversible[A <: Reversible[A]] { this: A =>
+  def rever() : A
+}
+sealed trait LinearDirection {
+  def addOneTo(from : Pos) : Option[Pos]
+}
 sealed trait StraightDirection extends LinearDirection
-sealed trait RankwiseDirection extends StraightDirection
-case object Inwards extends RankwiseDirection
-case object Outwards extends RankwiseDirection
-sealed trait FilewiseDirection extends StraightDirection
-case object Pluswards extends FilewiseDirection
-case object Minuswards extends FilewiseDirection
-case final class DiagonalDirection(val rankwise : RankwiseDirection, val filewise: FilewiseDirection)
+sealed trait RankwiseDirection extends StraightDirection, Reversible[RankwiseDirection]
+case object Inwards extends RankwiseDirection {
+  def rever = Outwards
+  def addOneTo(from: Pos) : Option[Pos] =
+    if (from.rank==5) Some(Pos(5, from.file.opposite)) else Some(Pos(from.rank+1, from.file))
+}
+case object Outwards extends RankwiseDirection {
+  def rever = Inwards
+  def addOneTo(from: Pos) : Option[Pos] =
+    if (from.rank==0) None else Some(Pos(from.rank-1, from.file))
+}
+sealed trait FilewiseDirection extends StraightDirection, Reversible[FilewiseDirection]
+case object Pluswards extends FilewiseDirection {
+  def rever = Minuswards
+  def addOneTo(from: Pos) : Option[Pos] = Some(Pos(from.rank, Pos.plus(from.file, 1)))
+}
+case object Minuswards extends FilewiseDirection {
+  def rever = Pluswards
+  def addOneTo(from: Pos) : Option[Pos] = Some(Pos(from.rank, Pos.minus(from.file, 1)))
+}
+case final class DiagonalDirection(val rankwise : RankwiseDirection,
+                                   val filewise: FilewiseDirection) {
+  def rever = DiagonalDirection(rankwise.rever, filewise.rever)
+  def addOneTo(from: Pos) : Option[Pos] = rankwise match {
+    case Inwards => from.rank match {
+      case 5 => Some(Pos(5, filewise match {
+        case Pluswards => Pos.minus(from.file, 10)
+        case Minuswards => Pos.plus(from.file, 10)
+      }))
+      case _ => Some(Pos(from.rank+1, Pos.signSum(filewise, from.file, 1)))
+    }
+    case Outwards => from.rank match {
+      case 0 => None
+      case _ => Some(Pos(from.rank-1), Pos.signSum(filewise, from.file, 1))
+    }
+  }
+}
 
+trait Vec {
+  def addTo(from : Pos) : Option[Pos]
+}
 class LinearVec[DirectionClass <: LinearDirection](val direction : DirectionClass,
                                                    val distance : Int Refined Positive)
+  extends Vec
+
+sealed trait Orientation {
+  val perpendicular : Orientation
+}
+case object Rankwise extends Orientation {
+  val perpendicular = Filewise
+}
+case object Filewise extends Orientation {
+  val perpendicular = Rankwise
+}
+
